12f7fbd4b8f86b7f028108c5fee6379a01039569
Dingwen Li <dingwenli@wustl.edu>
2018-06-04 14:30:09 -0500

    example codes

---
+++ SpirobankSmartSDKSample/app/src/main/java/com/spirometry/spirobanksmartsdksample/UrlConfig.java
@@ -0,0 +1,11 @@
+package com.spirometry.spirobanksmartsdksample;
+
+/**
+ * Created by dingwenli on 1/17/18.
+ */
+
+public class UrlConfig {
+    // Server upload url
+    public static String URL_FVC_UPLOAD = "http://172.16.10.165/spirometry/test_fvc_upload.php";
+    public static String URL_PEFFEV1_UPLOAD = "http://172.16.10.165/spirometry/test_peffev1_upload.php";
+}
