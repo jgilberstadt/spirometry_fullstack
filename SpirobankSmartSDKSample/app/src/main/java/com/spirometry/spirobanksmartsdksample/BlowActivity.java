79727cb6843506ed09dbb230e1cb24d74e06affa
Taehoon Bang <peterbang@wustl.edu>
2018-06-19 16:12:23 -0500

    BlowActivity Merge Conflict Fixed

--- SpirobankSmartSDKSample/app/src/main/java/com/spirometry/spirobanksmartsdksample/BlowActivity.java
+++ SpirobankSmartSDKSample/app/src/main/java/com/spirometry/spirobanksmartsdksample/BlowActivity.java
@@ -68,6 +68,10 @@ public class BlowActivity extends AppCompatActivity {
         Log.d("HYUNRAE", "running oncreate");
 
         mBundleData = new MyParcelable();
+        
+        blowDirection = (TextView) findViewById(R.id.blowDirection);
+        blowMessage = (TextView) findViewById(R.id.blowMessage);
+        numberCount = (TextView) findViewById(R.id.numberCount);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blow);
@@ -77,10 +81,6 @@ public class BlowActivity extends AppCompatActivity {
 
         //get device manager instance
         deviceManager = DeviceManager.getInstance(this);
-
-        blowDirection = (TextView) findViewById(R.id.blowDirection);
-        blowMessage = (TextView) findViewById(R.id.blowMessage);
-        numberCount = (TextView) findViewById(R.id.numberCount);
 
         //set device manger callback
         deviceManager.setDeviceManagerCallback(deviceManagerCallback);
