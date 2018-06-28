0fc0fcb39624db62a8de26367da261874963afd6
Taehoon Bang <peterbang@wustl.edu>
2018-06-28 16:56:07 -0500

    Notification Mostly Finished and Appointment Date Finished

---
+++ SpirobankSmartSDKSample/app/src/main/java/com/spirometry/spirobanksmartsdksample/NotifyService.java
@@ -0,0 +1,32 @@
+package com.spirometry.spirobanksmartsdksample;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+
+public class NotifyService extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.spiro)
+                .setContentTitle("Alarm activated")
+                .setContentText("alarm is this")
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
+                .setContentInfo("Info");
+
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, builder.build());
+
+    }
+}
