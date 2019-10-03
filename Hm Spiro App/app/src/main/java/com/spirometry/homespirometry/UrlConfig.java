7e39ee2afef5a70012b35d3603643ae85a73c32d
David Yang <yang.david@wustl.edu>
2019-10-03 10:56:27 -0500

    connection

--- Hm Spiro App/app/src/main/java/com/spirometry/homespirometry/UrlConfig.java
+++ Hm Spiro App/app/src/main/java/com/spirometry/homespirometry/UrlConfig.java
@@ -9,6 +9,6 @@ public class UrlConfig {
     static String URL_FVC_UPLOAD = "http://172.16.10.165/spirometry/test_fvc_upload.php";
     static String URL_PEFFEV1_UPLOAD = "http://172.16.10.165/spirometry/test_peffev1_upload.php";
     static String URL_VITOL_UPLOAD = "http://172.16.10.165/spirometry/vitol_upload.php";
-    static String URL_CHECK_FILE_EXIST = "https://ecp-app-hsdev1.nrg.wustl.edu/check_upload.php";
-    static String URL_CHECK_PATIENT_EXIST = "https://ecp-app-hsdev1.nrg.wustl.edu/spirometry/login.php";
+    static String URL_CHECK_FILE_EXIST = "https://hstest.wustl.edu/spirometry/check_upload.php";
+    static String URL_CHECK_PATIENT_EXIST = "https://hstest.wustl.edu/spirometry/login.php";
 }
