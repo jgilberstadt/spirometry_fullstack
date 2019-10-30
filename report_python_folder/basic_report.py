7d738999b5e1ecc124a5a04cd9cd311af8950f8c
Dingwen Li <dingwenli@wustl.edu>
2019-10-30 11:05:32 -0500

    Dingwen commit some python files used as the basic class for generating reports. For each individual report, we should extend the basic class and implement the methods accordingly.

---
+++ report_python_folder/basic_report.py
@@ -0,0 +1,48 @@
+# basic class for generating report
+
+# import libraries for PostgreSQL connection
+from dbconfig import config
+import psycopg2
+# import libraries for loading workbook templates
+from openpyxl import load_workbook
+
+class BasicReport:
+	def __init__(self, db_config_path=None, email_config_path=None, workbook_path=None, report_type=0):
+		# initialize the database connection 
+		# connect to PostgreSQL database
+		self.db_config_path = db_config_path
+		self.email_config_path = email_config_path
+		# get the database credentials from the .ini config file
+		dbc = config('database.ini' if self.db_config_path == None else self.db_config_path, 'postgresql')
+		self.cnx = psycopg2.connect(**dbc)
+
+		# initialize the workbook template that can be used for populating the values obtained 
+		# from the PostgreSQL table
+		self.workbook_path = 'Report_Templates/Report template - R6.xlsx' if workbook_path == None else workbook_path
+
+		# initialize the report type which can be used for selecting the corresponding worksheet.
+		# 0: Report Datasheet (Monthly)
+		# 1: Report Datasheet M (V+AMI-P+)
+		# 2: Report Datasheet (V+AMI-P+)
+		# 3: Report Datasheet (V+AMI-P-)
+		# 4: Report Datasheet (V+AMI+-)
+		# 5: Report Datasheet (Conv 10 wks)
+		# 6: Report Datasheet (Conv 6 wks)
+		self.report_type_dict = {0:'Report Datasheet (Monthly)', 1:'Report Datasheet M (V+AMI-P+)',
+		2:'Report Datasheet (V+AMI-P+)', 3:'Report Datasheet (V+AMI-P-)', 4:'Report Datasheet (V+AMI+-)',
+		5:'Report Datasheet (Conv 10 wks)', 6:'Report Datasheet (Conv 6 wks)'}
+		self.report_type_name = self.report_type_dict[report_type]
+
+	# Plot the graphs and place them in the spreedsheet
+	def plotGraph(self):
+		print("This is the basic class method, please implement it in the subclass")
+
+	# Query the PostgreSQL for personalized values to replace the values in the report template
+	def replaceValues(self):
+		print("This is the basic class method, please implement it in the subclass")
+
+	def generateReport(self):
+		print("This is the basic class method, please implement it in the subclass")
+
+
+
