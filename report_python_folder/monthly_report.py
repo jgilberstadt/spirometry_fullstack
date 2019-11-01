0632a17abeffcf6a3eecdd2d86b7f619066def4a
Dingwen Li <dingwenli@wustl.edu>
2019-10-31 23:22:20 -0500

    Dingwen's first attempt to generate a report via using the monthly_report.py

--- report_python_folder/monthly_report.py
+++ report_python_folder/monthly_report.py
@@ -9,18 +9,30 @@ from openpyxl import load_workbook
 from basic_report import BasicReport
 
 class MonthlyReport(BasicReport):
-	def __init__(self):
-		super().__init__(db_config_path='../credential/database.ini', 
+	def __init__(self, imei_num, current_month):
+		BasicReport.__init__(self, db_config_path='credentials/database.ini', 
 			workbook_path='Report_Templates/Report template - R6.xlsx', report_type=0)
 
 		# put report-specific initalization here
 		# e.g. graph objects, replaced values queried from DB
+		self.imei_num = imei_num
+		self.current_month = current_month
+		self.ws = self.wb['Report Datasheet (Monthly)']
 
 	# Plot the graphs and place them in the spreedsheet
 	def plotGraph(self):
+		print("Need implementation")
 
 	# Query the PostgreSQL for personalized values to replace the values in the report template
 	def replaceValues(self):
+		# query patient study number
+		patient_study_number_cursor = self.cnx.cursor()
+		patient_study_number_cursor.execute('SELECT patient_id FROM patient_data WHERE imei_num=%s', (self.imei_num))
+		for patient_study_number in patient_study_number_cursor:
+			self.ws['E2'] = patient_study_number
+			print patient_study_number
+			break
 
-# If everything is taken care by the above two methods, we can just save the xlsx in this method
+	# If everything is taken care by the above two methods, we can just save the xlsx in this method
 	def generateReport(self):
+		self.wb.save(filename = self.imei_num+"_Monthly_Report_"+str(self.current_month)+".xlsx")
--- report_python_folder/test_auto.py
+++ report_python_folder/test_auto.py
@@ -1,6 +1,13 @@
+"""
 from openpyxl import load_workbook
 wb = load_workbook(filename = 'Report template - R6.xlsx')
 ws1 = wb['Report Datasheet (Monthly)']
 print ws1['C27'].value
 
 wb.save(filename = 'Report save.xlsx')
+"""
+
+from monthly_report import MonthlyReport
+
+mr_obj = MonthlyReport(imei_num='176927',current_month=1)
+mr_obj.generateReport()
